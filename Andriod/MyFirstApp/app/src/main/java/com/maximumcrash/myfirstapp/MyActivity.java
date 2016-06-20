278bdedc9ac6f498bd80ed6697824eb1102b1a33
Rejon Taylor-Foster <maximumcrash@hotmail.com>
2016-06-20 15:31:32 -0400

    Babysteps
    -Learning my way around the IDE
    -Finished the first Android tutorial

---
+++ Andriod/MyFirstApp/app/src/main/java/com/maximumcrash/myfirstapp/MyActivity.java
@@ -0,0 +1,24 @@
+package com.maximumcrash.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.maximumcrash.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+}
